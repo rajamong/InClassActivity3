a37727dc5a65a44f713a1cd2aa138d8fea336e91
rajamong <63121185+rajamong@users.noreply.github.com>
2021-04-30 15:22:12 -0700

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,28 +1,33 @@
-# addition function
-def addition(num1, num2):
-    return num1 + num2
+# creates a class called calculator
+class calculator:
+    # creates an empty constructor
+    def __init__(self):
+        pass
+    # addition function
+    def addition(self, num1, num2):
+        return num1 + num2
 
-# subtraction function
-def subtraction(num1, num2):
-    return num1 - num2
+    # subtraction function
+    def subtraction(self, num1, num2):
+        return num1 - num2
 
-# multiplication function
-def multiplication(num1, num2):
-    return num1 * num2
+    # multiplication function
+    def multiplication(self, num1, num2):
+        return num1 * num2
 
-# division function
-def division(num1, num2):
-    return num1 / num2
+    # division function
+    def division(self, num1, num2):
+        return num1 / num2
 
 
-while True:
-
+#while True:
+    
     # take in the users inputs
-    num1 = float(input("pls enter your first number: "))
-    num2 = float(input("pls enter your second number: "))
+    #num1 = float(input("pls enter your first number: "))
+    #num2 = float(input("pls enter your second number: "))
 
-    # prints out all of the calculations
-    print(num1, "+", num2, "=", addition(num1, num2))
-    print(num1, "-", num2, "=", subtraction(num1, num2))
-    print(num1, "*", num2, "=", multiplication(num1, num2))
-    print(num1, "/", num2, "=", division(num1, num2))
+   # prints out all of the calculations
+   # print(num1, "+", num2, "=", addition(num1, num2))
+   # print(num1, "-", num2, "=", subtraction(num1, num2))
+   # print(num1, "*", num2, "=", multiplication(num1, num2))
+   # print(num1, "/", num2, "=", division(num1, num2))
